f43e3ab07175f3c20bf35d2e985a9631350c1b51
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-09-30 14:13:05 +0700

    input vector

--- seven_dwarves/7dwarves.cpp
+++ seven_dwarves/7dwarves.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
-#include <set>
+#include <vector>
 using namespace std;
 
 int main(){
     int n = 9;
-    int num[n];
+    int k = 7;
+    vector<int> num;
+    vector<vector<int>> comb;
+    //input nine number
     for (int i = 0; i < n; i++){
-       cin >> num[i]; 
+        int input;
+       cin >> input; 
+       num.push_back(input);
     }
 
-    
-
-
-
+    for (int x : num){
+        cout << x << endl;
+    }
 
 
     return 0;
