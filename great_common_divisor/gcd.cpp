284d0ee55ecf630d2a91ed3c550bdfcfeb458e78
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-09-30 21:58:07 +0700

    complete 100 p

--- great_common_divisor/gcd.cpp
+++ great_common_divisor/gcd.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 
 int main(){
+    // input two int
+    int a,b,n,gcd,d = 1;
+    cin >> a >> b;
+
+    // check if n = a or b
+    if (a >= b){
+        n = b;
+    }else if (b >= a){
+        n = a;
+    }
+
+    // make for loop that increment number and try to devide by them two
+    for (int i = 0; i < n; i++)
+    {
+        if (a%d == 0 && b%d == 0){
+                gcd = d;
+        }
+        d++;
+    }
     
+    // out put great common divisor
+    cout << gcd;
 
     return 0;
 }
