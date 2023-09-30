670ed912bf0a24c5f99ffd17cb26788db0d5c2df
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-09-30 22:08:02 +0700

    init

---
+++ ptice/ptice.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    // input stirngs as vector
+    // string act similar to array
+    string key; 
+    cin >> key;
+
+    // Adrian = A B C loop
+    // Bruno = B A B C loop
+    // Goran = C C A A B B loop
+
+    return 0;
+}
