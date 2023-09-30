959f38001825690ede37f3b380c69820c7f8772d
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-09-30 14:25:21 +0700

    recursive function

--- seven_dwarves/7dwarves.cpp
+++ seven_dwarves/7dwarves.cpp
@@ -2,22 +2,39 @@
 #include <vector>
 using namespace std;
 
+void findCombinations(vector<int>& num, vector<int>& combination, int i, int k,vector<vector<int>> result){
+
+    if(k==0){
+        result.push_back(combination);
+        return;
+    }
+    
+    if (i == num.size()){
+        return;
+    }
+    
+
+    combination.push_back(num[i]);
+    findCombinations(num, combination, i+1, k-1, result);
+
+    combination.pop_back();
+    findCombinations(num,combination,i+1,k-1, result);
+
+}
+
+
+
 int main(){
     int n = 9;
     int k = 7;
     vector<int> num;
-    vector<vector<int>> comb;
+    vector<int> combination;
+    vector<vector<int>> result;
     //input nine number
     for (int i = 0; i < n; i++){
         int input;
        cin >> input; 
        num.push_back(input);
     }
-
-    for (int x : num){
-        cout << x << endl;
-    }
-
-
     return 0;
 }
