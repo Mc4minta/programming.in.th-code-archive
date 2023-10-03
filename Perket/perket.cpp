f7c8596d19743f192b1a71ae0fdbb01714dde1d6
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-03 10:23:05 +0700

    Update perket.cpp

--- Perket/perket.cpp
+++ Perket/perket.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+void printTaste(vector<vector<int> > taste){
+	cout << "Your taste are: " << endl;
+	for (vector<int> sb : taste){
+		for (int x : sb){
+			cout << x << " ";
+		}
+		cout << endl;
+	}
+}
+
 int main(){ 
 	// input N
 	int n;
@@ -12,7 +22,7 @@ int main(){
 	vector< vector<int> > taste;
 	int s,b;
 	vector<int> sb;
-	cout << "Enter sout and bitter:" << endl;
+	cout << "Enter sour and bitter:" << endl;
 	for (int inputRow = 0; inputRow < n; inputRow++){
 		cin >> s >> b;
 		sb.push_back(s);
@@ -20,13 +30,12 @@ int main(){
 		taste.push_back(sb);
 		sb.clear();
 	}
-	cout << "Your taste are: " << endl;
-	for (vector<int> sb : taste){
-		for (int x : sb){
-			cout << x << " ";
-		}
-		cout << endl;
-	}
+	
+	// printTaste(taste);
+	
+	// calculate sour and bitter
+	
+	
 	
 	return 0;
 }
