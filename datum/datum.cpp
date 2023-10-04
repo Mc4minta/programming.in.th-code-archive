357c737a41964580374da41392d25c707e98e481
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-04 16:40:31 +0700

    draft

--- datum/datum.cpp
+++ datum/datum.cpp
@@ -14,5 +14,20 @@ int main(){
     cout << "gap before add is " << gap << endl;
     gap+=(d-1);
     cout << "gap after add is " << gap << endl;
+    // print day
+    int r = gap%7;
+    cout << "gap mod 7 is " << r << endl;
+    //string day[7] = {"Thursday","Friday","Saturday","Sunday","Monday","Tuesday","Wednesday"};
+    map <int, string> ans;
+    ans[0] = "Thursday";
+    ans[1] = "Friday";
+    ans[2] = "Saturday";
+    ans[3] = "Sunday";
+    ans[4] = "Monday";
+    ans[5] = "Tuesday";
+    ans[6] = "Wednesday";
+
+    cout << ans[r];
+    
     return 0;
 }
