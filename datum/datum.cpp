a91ad5d8a8b2da711682257a9d0c45d107f4702a
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-04 16:27:00 +0700

    edit datum

---
+++ datum/datum.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int d,m;
+    cout << "Enter D and M: " << endl;
+    cin >> d >> m;
+    // calculate date from first of year
+    return 0;
+}
