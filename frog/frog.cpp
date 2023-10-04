1b191dd85f9a044d8a76a5d1519a1e7f01b6cc2c
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-04 19:02:55 +0700

    sumitted 100 p

--- frog/frog.cpp
+++ frog/frog.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int x,y;
+    cin >> x >> y;
+    int times=0;
+    int distance=0;
+    if (x <= y){
+        while (distance<y){
+                distance+=x;
+                times++;
+            }
+    }else{
+        times = 2;
+    }
+    cout << times;
 }
