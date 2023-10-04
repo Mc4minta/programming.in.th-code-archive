abcd0408f189d3244f07162806de4f10a9d711bf
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-04 16:33:44 +0700

    done calculating gap day

--- datum/datum.cpp
+++ datum/datum.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main(){
     int d,m;
+    int gap = 0;
     cout << "Enter D and M: " << endl;
     cin >> d >> m;
     // calculate date from first of year
+    int dayInMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    for (int i=0; i<(m-1); i++){
+        gap+=dayInMonth[i];
+    }
+    cout << "gap before add is " << gap << endl;
+    gap+=(d-1);
+    cout << "gap after add is " << gap << endl;
     return 0;
 }
