d8723f85eba361c8ae792b5c54d27eadc81b261c
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-08 13:22:39 +0700

    Update primeOdd.cpp

--- prime-odd/primeOdd.cpp
+++ prime-odd/primeOdd.cpp
@@ -1,38 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool primeCheck(int num[], int index){
-	bool prime = true;
-	double sq = sqrt(num[index]);
-	int i;
-	if (num[index] == 2) return true;
-	for (i=2; i<=sq; i++){
-		if (num[index]%i == 0){
-			prime = false;
-		}
-	}
-	return prime;
-}
-
-
 int main(){
 	int n; cin >> n;
-	int num[n];
+	string num[n];
 	int i;
 	for (i=0; i<n; i++) cin >> num[i];
-	if (primeCheck(num,i)){
-		cout << "T";
-	}else if (num[i]%2 != 0){
-		cout << "T";
-	}else if (num[i]%2 == 0){
-		cout << "F";
-	}else {
-		cout << "F";
+	for (i=0; i<n; i++){
+		int m = num[i].length();
+		if (num[i] == "2"){
+			cout << "T" << endl;
+		}else if (num[i][m-1]%2 == 0){
+			cout << "F" << endl;
+		}else{
+			cout << "T" << endl;
+		}
 	}
 }
 
-	//	if (num[i] == 2 or num[i] == 3){
-	//		cout << "T";
-	//	}else if (num[i]+1%6 == 0 or num[i]-1%6 == 0){
-	//		cout << "T";
-	//	}else if ()
+	// if == '2' then true, if last digit can be divided by 2 then false, else true
