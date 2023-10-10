1a2aac0eed2c2f30185e83dc14f2853a867f8019
Mc4minta <142011388+Mc4minta@users.noreply.github.com>
2023-10-10 16:04:05 +0700

    Create primeodd.cpp

---
+++ COMPLETED/primeodd_COMPLETED/primeodd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n; cin >> n;
+	string num[n];
+	int i;
+	for (i=0; i<n; i++) cin >> num[i];
+	for (i=0; i<n; i++){
+		int m = num[i].length();
+		if (num[i] == "2"){
+			cout << "T" << endl;
+		}else if (num[i][m-1]%2 == 0){
+			cout << "F" << endl;
+		}else{
+			cout << "T" << endl;
+		}
+	}
+}
